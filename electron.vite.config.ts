917fa7ddae792fd3aed652d9df51ead1bb829a1f
Imam Nur <imamnurfahudi@gmail.com>
2024-04-05 07:43:24 +0700

    first commit

---
+++ electron.vite.config.ts
@@ -0,0 +1,28 @@
+import { resolve } from 'path'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import vue from '@vitejs/plugin-vue'
+
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    css: {
+      postcss: {
+        plugins: [tailwind(), autoprefixer()]
+      }
+    },
+    resolve: {
+      alias: {
+        '@': resolve('src/renderer/src')
+      }
+    },
+    plugins: [vue()]
+  }
+})
---
+++ src/renderer/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <div>
+    <Button>Button</Button>
+  </div>
+</template>
+
+<style scoped></style>
